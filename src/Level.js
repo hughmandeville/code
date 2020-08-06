12b8ba140aeeee2178dc4d6084161b79a0623202
nyt-hughmandeville <hubert.mandeville@nytimes.com>
2020-08-06 12:34:26 -0400

    added alt

--- src/Level.js
+++ src/Level.js
@@ -33,6 +33,7 @@ const Level = ({ level, levels, setLevel }) => {
           showHint={showHint}
           setShowHint={setShowHint}
         />
+        <div id="title">🤓 🤪 🤓 🤪 🤓 🤪 🤓</div>
         <div id="boxes" className="won">
           Congratulations you completed all the challenges!
         </div>
@@ -50,9 +51,7 @@ const Level = ({ level, levels, setLevel }) => {
         showHint={showHint}
         setShowHint={setShowHint}
       />
-      <div id="title" alt="">
-        {image ? <img src={image} /> : title}
-      </div>
+      <div id="title">{image ? <img src={image} alt="" /> : title}</div>
       <div id="hint">{showHint ? <span>{hint}</span> : ""}</div>
       <div id="boxes">
         {puzzle.slice(0, completed).map((n, i) => (
