9edd3878347b2c11bb05668e939c8af1626fcf26
nyt-hughmandeville <hubert.mandeville@nytimes.com>
2020-08-06 15:41:46 -0400

    Added timer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { useState } from "react";
 import Level from "./Level";
+import Timer from "./Timer";
 import "./index.css";
 
 const App = () => {
   const levels = getLevels();
   const [level, setLevel] = useState(0);
-  return <Level level={level} levels={levels} setLevel={setLevel} />;
+  return (
+    <>
+      <Level level={level} levels={levels} setLevel={setLevel} />
+      <Timer />
+    </>
+  );
 };
 
 // construct array of levels.
